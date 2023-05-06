e08ca61aeef38c5e097c7fd44e5789f3d30743c7
calebthewood <calebwood.cs@gmail.com>
2023-05-06 15:31:58 -0700

    Add Mock data

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# Grond: A Workout App
+### AKA Karissa's weekend project.
+
+Grond is a Card-based workout app. It allows you to create workout cards for excercises like push-ups, squats, lunges, etc., and assemble them into decks with additional rules such as the number of reps per exercise or timed intervals. Users will be able to create their own decks and cards or choose from premade decks.
---
+++ src/MockData.tsx
@@ -0,0 +1,41 @@
+/* Mock Data for Development. To be Replaced by Node-Express BE. */
+
+interface ICard {
+  title: string;
+  description: string;
+  image: string;
+  rules?: {
+    qty: number | null;
+    seconds: number | null;
+  };
+}
+
+export const CardList: ICard[] = [
+  {
+    title: 'Push-ups',
+    description: 'Keeping your elbows close, and torso rigid, dip until your chest nearly touches the ground',
+    image: '../assets/pushup.png',
+  },
+  {
+    title: 'Overhand Pull-ups',
+    description: 'Grab the bar palms facing away, sink until your arms are fully extended, then pull yourself up until your chin is above the bar.',
+    image: '../assets/pullup.png',
+  },
+  {
+    title: 'Squats',
+    description: 'Plant feet shoulder\'s width apart. Dip down, getting your bum as close to the ground as your legs allow. Ass to grass!',
+    image: '../assets/squat.png',
+  },
+];
+
+interface IDeck {
+  title: string;
+  description: string;
+  exercises: ICard[];
+}
+
+export const TestDeck: IDeck = {
+  title: 'Test Deck 1',
+  description: 'This workout deck contains pushups, pullups, and squats.',
+  exercises: CardList
+};
