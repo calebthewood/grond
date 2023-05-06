2ec4d23d1df18747b0f9b0e779e2f68d2617e8c2
calebthewood <calebwood.cs@gmail.com>
2023-05-06 11:09:23 -0700

    Adds basic err handling

--- src/Card/CreateCard.tsx
+++ src/Card/CreateCard.tsx
@@ -1,38 +1,74 @@
 import { useState } from "react";
 
+interface IFormData {
+  title: string;
+  description: string;
+  image: string;
+}
+
 /** CreateCard
  * Form component used to enter a new workout card for use in a deck
  */
 export function CreateCard() {
 
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<IFormData>({
     title: '',
     description: '',
     image: '',
   });
-  const [formErrors, setFormErrors] = useState([]);
+
+  const [formErrors, setFormErrors] = useState<IFormData>({
+    title: '',
+    description: '',
+    image: ''
+  });
 
   /** Handle form data changing */
-  function handleChange(evt) {
+  function handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = evt.target;
     setFormData(f => ({
       ...f,
       [name]: value,
     }));
-    setFormErrors([]);
+    // reset the error msg once field is typed in.
+    setFormErrors(f => ({
+      ...f,
+      [name]: ''
+    }));
+  }
+
+  /** Handle form submit
+   * - place in local storage for now?
+   * - or add it to state at app level?
+   */
+  function handleSubmit(evt: React.FormEvent) {
+    evt.preventDefault();
+
+    // expand this to be cover more cases than just empty.
+    for (const [field, value] of Object.entries(formData)) {
+      if (value === '') {
+        setFormErrors({ ...formErrors, [field]: 'Text Required' });
+      }
+    }
+    if (Object.values(formErrors).some((value) => value.length > 0))
+      console.log('Put this somewhere', formData);
   }
 
   return (
     <div>
-      <form action="" method="post">
+      <h2>Create a new exercise</h2>
+      <form onSubmit={handleSubmit} action="" method="post">
+        <p>{formErrors.title}</p>
         <label htmlFor="">Workout Title
-          <input type="text" name="title" id="" value={formData.title} />
+          <input type="text" name="title" id="" onChange={handleChange} value={formData.title} />
         </label>
+        <p>{formErrors.description}</p>
         <label htmlFor="">Workout Description
-          <input type="text" name="description" id="" value={formData.description} />
+          <input type="text" name="description" id="" onChange={handleChange} value={formData.description} />
         </label>
+        <p>{formErrors.image}</p>
         <label htmlFor="">Workout Image
-          <input type="text" name="image" id="" value={formData.image} />
+          <input type="text" name="image" id="" onChange={handleChange} value={formData.image} />
         </label>
         <button>Add New Workout!</button>
       </form>
