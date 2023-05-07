76d3f1ffad15d40d10ca371c7ae3773e314af217
calebthewood <calebwood.cs@gmail.com>
2023-05-06 22:47:17 -0700

    Adds props interface

--- src/Card/CardList.tsx
+++ src/Card/CardList.tsx
@@ -1,29 +1,10 @@
 import { useState } from "react";
 import { WorkoutCard } from "./WorkoutCard";
+import { cardList } from "../MockData";
 
-interface ICard {
-  title: string;
-  description: string;
-  image: string;
+interface ICardLostProps {
+  topLevelNav: (to: string) => void;
 }
-
-const mockCards = [
-  {
-    title: 'Push-ups',
-    description: 'Keeping your elbows close, and torso rigid, dip until your chest nearly touches the ground',
-    image: '../assets/pushup.png',
-  },
-  {
-    title: 'Overhand Pull-ups',
-    description: 'Grab the bar palms facing away, sink until your arms are fully extended, then pull yourself up until your chin is above the bar.',
-    image: '../assets/pullup.png',
-  },
-  {
-    title: 'Squats',
-    description: 'Plant feet shoulder\'s width apart. Dip down, getting your bum as close to the ground as your legs allow. Ass to grass!',
-    image: '../assets/squat.png',
-  },
-];
 
 /** Card List
  * - Will be rendered in the following places
@@ -36,12 +17,14 @@ const mockCards = [
  *       -- needs option to edit
  *
  */
-export function CardList() {
+export function CardList({ topLevelNav }: ICardLostProps) {
   // long term: cards will be fetched
-  const [cards, setCards] = useState<ICard[]>(mockCards);
+  const [cards, setCards] = useState(cardList);
 
-  return (<>
-    <h2>Workout Cards</h2>
-    {cards.map(card => <WorkoutCard workout={card} />)}
-  </>);
+  return (
+    <>
+      <h2>Workout Cards</h2>
+      {cards.map(card => <WorkoutCard workout={card} />)}
+    </>
+    );
 }
