e5e2c408758c912c5f867df108e36d42693c9ba9
calebthewood <calebwood.cs@gmail.com>
2023-05-06 23:50:08 -0700

    Set up mock data

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,45 @@ import './App.css';
 import { WorkoutSet } from './Workout/WorkoutSet';
 import { DeckList } from './Deck/DeckList';
 import { CardList } from './Card/CardList';
+import { testDeck, IDeck } from './MockData';
+
+
+
+/* Views:
+Default - render DeckList
+  - select Deck -> WorkoutSet
+  - select CreateDeck -> CreateDeckForm
+
+WorkoutSet - renders WorkoutSet, on completion, returns to default
+  - a carousel of cards, getting to end -> default
+  - select X btn ->  returns to default
+*/
 
 function App() {
-  /* Views:
-  Default - render DeckList
-    - select Deck -> WorkoutSet
-    - select CreateDeck -> CreateDeckForm
 
-  WorkoutSet - renders WorkoutSet, on completion, returns to default
-    - a carousel of cards, getting to end -> default
-    - select X btn ->  returns to default
-  */
-  const [activeComponent, setActiveComponent] = useState('workout');
+  const [activeComponent, setActiveComponent] = useState('decks');
+  const [decks, setDecks] = useState<IDeck[]>([testDeck, testDeck, testDeck]);
+  const [activeDeck, setActiveDeck] = useState<IDeck>(decks[0]);
+
+  function selectWorkoutDeck(deckIdx: number): void {
+    setActiveDeck(decks[deckIdx]);
+  }
 
   const renderComponent = () => {
     switch (activeComponent) {
       case 'workout':
-        return <WorkoutSet topLevelNav={topLevelNav} />;
+        return <WorkoutSet deck={activeDeck} topLevelNav={topLevelNav} />;
       case 'decks':
-        return <DeckList topLevelNav={topLevelNav}  />;
+        return <DeckList selectDeck={selectWorkoutDeck} decks={decks} topLevelNav={topLevelNav} />;
       case 'cards':
-        return <CardList topLevelNav={topLevelNav}  />;
+        return <CardList topLevelNav={topLevelNav} />;
       default:
         return null;
     }
   };
 
   function topLevelNav(to: string) {
-    setActiveComponent(to)
+    setActiveComponent(to);
   }
 
   return (
