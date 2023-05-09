d574eef028392de3bbf9d2633e2577cbe9419303
calebthewood <calebwood.cs@gmail.com>
2023-05-08 17:11:35 -0700

    adds next prev functionality

--- src/Workout/WorkoutSet.tsx
+++ src/Workout/WorkoutSet.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { WorkoutCard } from "../Card/WorkoutCard";
 import { IDeck, ICard } from "../MockData";
+import './workout.css'
 
 
 interface IWorkoutSetProps {
@@ -18,10 +19,28 @@ export function WorkoutSet({ topLevelNav, deck }: IWorkoutSetProps) {
     topLevelNav('decks');
   }
 
+  function next() {
+    setIndex(index < cards.length - 1 ? index + 1 : index);
+  }
+
+  function prev() {
+    setIndex(index > 0 ? index - 1 : 0);
+  }
+
   return (
     <section>
       <button onClick={returnHome}>X</button>
-      <WorkoutCard workout={cards[index]} />
+      <div className="set-wrapper">
+        <button
+          disabled={index <= 0}
+          onClick={prev}
+          className="workout-nav">←</button>
+        <WorkoutCard workout={cards[index]} />
+        <button
+          disabled={index >= cards.length - 1}
+          onClick={next}
+          className="workout-nav">→</button>
+      </div>
     </section>
   );
 }
