cc0b30bec18deaab318928660d2adee4523d401d
calebthewood <calebwood.cs@gmail.com>
2023-05-08 18:34:35 -0700

    Moves info boolean up to workoutSet

--- src/Workout/WorkoutSet.tsx
+++ src/Workout/WorkoutSet.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { WorkoutCard } from "../Card/WorkoutCard";
 import { IDeck, ICard } from "../MockData";
-import './workout.css'
+import './workout.css';
 
 
 interface IWorkoutSetProps {
@@ -14,30 +14,43 @@ export function WorkoutSet({ topLevelNav, deck }: IWorkoutSetProps) {
 
   const [cards, setCards] = useState<ICard[]>(deck.exercises);
   const [index, setIndex] = useState<number>(0);
+  const [showInfo, setShowInfo] = useState(false);
 
   function returnHome() {
     topLevelNav('decks');
   }
 
   function next() {
+    if (index >= cards.length - 1) {
+      topLevelNav('decks');
+      // add a "finished!" screen b/f returning to decks
+      return;
+    }
     setIndex(index < cards.length - 1 ? index + 1 : index);
+    setShowInfo(false);
   }
 
   function prev() {
     setIndex(index > 0 ? index - 1 : 0);
+    setShowInfo(false);
+  }
+
+  function toggleShowInfo() {
+    setShowInfo(!showInfo);
   }
 
   return (
     <section>
-      <button onClick={returnHome}>X</button>
+      <div className="return-btn">
+        <button onClick={returnHome}>End Workout</button>
+      </div>
       <div className="set-wrapper">
         <button
           disabled={index <= 0}
           onClick={prev}
           className="workout-nav">←</button>
-        <WorkoutCard workout={cards[index]} />
+        <WorkoutCard workout={cards[index]} showInfo={showInfo} toggleShowInfo={() => setShowInfo(!showInfo)} />
         <button
-          disabled={index >= cards.length - 1}
           onClick={next}
           className="workout-nav">→</button>
       </div>
