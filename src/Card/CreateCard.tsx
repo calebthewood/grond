52b6c774af4479f8394bbe1ed280d7cb36fdc063
calebthewood <calebwood.cs@gmail.com>
2023-05-10 21:30:30 -0700

    Breaks form into two sub components

--- src/Card/CreateCard.tsx
+++ src/Card/CreateCard.tsx
@@ -1,125 +1,52 @@
 import { useState } from "react";
+import { ICard } from "../MockData";
+import { SideAForm } from "./SideAForm";
+import { SideBForm } from "./SideBForm";
 
-interface IFormData {
+
+interface ICreateCardProps {
+  addCard: (c: ICard) => void;
+}
+
+export interface ISideAForm {
   title: string;
-  description: string;
   image: string;
-  steps: string[];
+}
+
+export interface ISideBForm {
+  steps: string[]
 }
 
 /** CreateCard
  * Form component used to enter a new workout card for use in a deck
  */
-export function CreateCard() {
+export function CreateCard({ addCard }: ICreateCardProps) {
 
-  const [partA, setPartA] = useState(true);
-  const [formData, setFormData] = useState<IFormData>({
+  const [showA, setShowA] = useState(true);
+  const [newCard, setNewCard] = useState<ICard>({
     title: '',
-    description: '',
     image: '',
-    steps: [''],
+    steps: [],
+    rules: {
+      qty: null,
+      seconds: null
+    }
   });
 
-  const [formErrors, setFormErrors] = useState<IFormData>({
-    title: '',
-    description: '',
-    image: '',
-    steps: [''],
-  });
-
-  /** Handle form data changing */
-  function handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = evt.target;
-    setFormData(f => ({
-      ...f,
-      [name]: value,
-    }));
-    // reset the error msg once field is typed in.
-    setFormErrors(f => ({
-      ...f,
-      [name]: ''
-    }));
-  }
-
-  /** Handle form submit
-   * - place in local storage for now?
-   * - or add it to state at app level?
-   */
-  function handleSubmit(evt: React.FormEvent) {
-    evt.preventDefault();
-    console.debug('handleSubmit');
-    // expand this to be cover more cases than just empty.
-    for (const [field, value] of Object.entries(formData)) {
-      if (value === '') {
-        setFormErrors({ ...formErrors, [field]: 'Text Required' });
-      }
-    }
-    if (Object.values(formErrors).some((value) => value.length > 0)) {
-      console.log('Put this somewhere', formData);
-    }
-
-  }
-
-  function addStep() {
-    const newData = formData;
-    newData.steps.push('');
-    setFormData(newData);
-  }
-
-  function removeStep() {
-    const newData = formData;
-    newData.steps.pop();
-    setFormData(newData);
+  function updateNewCard(formData: ISideAForm | ISideBForm) {
+    setNewCard({
+      ...newCard,
+      ...formData
+    });
   }
 
   return (
     <div>
       <h2>Create a new exercise</h2>
-      <form onSubmit={handleSubmit} action="" method="post">
-        {partA ?
-          <>
-            <h3>Side A: Details</h3>
-            <p>{formErrors.title}</p>
-            <label htmlFor="">Title
-              <input type="text" name="title" id="title" onChange={handleChange} value={formData.title} />
-            </label>
-            <p>{formErrors.description}</p>
-            <label htmlFor="">Description
-              <input type="text" name="description" id="description" onChange={handleChange} value={formData.description} />
-            </label>
-            <p>{formErrors.image}</p>
-            <label htmlFor="">Image
-              <input type="text" name="image" id="image" onChange={handleChange} value={formData.image} />
-            </label>
-            <button onClick={() => setPartA(false)}>Next: Add Steps</button>
-          </> : <>
-            <h3>Side B: Steps</h3>
+      {showA ?
+        <SideAForm toggleForm={()=> setShowA(!showA)} updateNewCard={updateNewCard} />
+        : <SideBForm updateNewCard={updateNewCard} />}
 
-            {formData.steps.map((step, i) =>
-              <>
-                <p>{formErrors.steps[i]}</p>
-                <label htmlFor="">{`Step ${(++i)}`} </label>
-                <input
-                  type="text"
-                  name={`step-${i}`}
-                  id={`step-${i}`}
-                  onChange={handleChange}
-                  value={formData.steps[i]} />
-              </>
-            )}
-            <div>
-              <button
-                disabled={!formData.steps.length}
-                onClick={removeStep}>- Step</button>
-              <button
-                disabled={formData.steps.length >= 10}
-                onClick={addStep}>+ Step</button>
-            </div>
-            <button onClick={handleSubmit}>Done: Add Card!</button>
-          </>}
-
-
-      </form>
     </div>
   );
 }
